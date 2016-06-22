661b06671ecdf319fc107212449620c2c9689d21
Clair Griffiths and Jonathan Lim <clairgriffiths+jonlim@notonthehighstreet.com>
2016-06-22 13:17:33 +0100

    Day 2 refactor out small smells

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -9,15 +9,46 @@ class Bottles
   end
 
   def verse(number)
-    case number
-    when 0
-      "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"
-    when 1
-      "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n"
-    when 2
-      "2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n"
+    "#{how_many(number).capitalize} #{beer_holder(number)} of beer on the wall, #{how_many(number)} #{beer_holder(number)} of beer.\n#{mission(number)}, #{how_many(succ(number))} #{beer_holder(succ(number))} of beer on the wall.\n"
+  end
+
+  def mission(number)
+    if number == 0
+      "Go to the store and buy some more"
     else
-      "#{number} bottles of beer on the wall, #{number} bottles of beer.\nTake one down and pass it around, #{number-1} bottles of beer on the wall.\n"
+      "Take #{consignment(number)} down and pass it around"
+    end
+  end
+
+  def succ(number)
+    if number == 0
+      99
+    else
+      number - 1
+    end
+  end
+
+  def beer_holder(number)
+    if number == 1
+      "bottle"
+    else
+      "bottles"
+    end
+  end
+
+  def consignment(number)
+    if number == 1
+      "it"
+    else
+      "one"
+    end
+  end
+
+  def how_many(number)
+    if number == 0
+      "no more"
+    else
+      number.to_s
     end
   end
 
