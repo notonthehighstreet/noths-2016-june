8a8b1e29af7298b592a7591210ff71fc45b29679
Jonathan Lim and Mazin Power <jonlim+mazinpower@notonthehighstreet.com>
2016-06-24 11:50:29 +0100

    STUFF

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -9,12 +9,25 @@ class Bottles
   end
 
   def verse(number)
-    container_number      = ContainerNumber.new(number)
-    next_container_number = ContainerNumber.new(container_number.successor)
-    "#{container_number.amount.capitalize} #{container_number.container} of beer on the wall, " +
-    "#{container_number.amount} #{container_number.container} of beer.\n" +
+    container_number      = container_number_factory(number)
+    next_container_number = container_number_factory(container_number.successor)
+    "#{container_number.capitalize} of beer on the wall, " +
+    "#{container_number} of beer.\n" +
     "#{container_number.action}, " +
-    "#{next_container_number.amount} #{next_container_number.container} of beer on the wall.\n"
+    "#{next_container_number} of beer on the wall.\n"
+  end
+
+  def container_number_factory(number)
+    case number
+    when 0
+      ContainerNumber0
+    when 1
+      ContainerNumber1
+    when 6
+      ContainerNumber6
+    else
+      ContainerNumber
+    end.new(number)
   end
 end
 
@@ -25,43 +38,61 @@ class ContainerNumber
     @number = number
   end
 
+  def to_s
+    "#{amount} #{container}"
+  end
+
+  def capitalize
+    to_s.capitalize
+  end
+
   def amount
-    if number == 0
-      "no more"
-    else
-      number.to_s
-    end
+    number.to_s
   end
 
   def container
-    if number == 1
-      "bottle"
-    else
-      "bottles"
-    end
+    "bottles"
   end
 
   def action
-    if number == 0
-      "Go to the store and buy some more"
-    else
-      "Take #{pronoun} down and pass it around"
-    end
+    "Take #{pronoun} down and pass it around"
   end
 
   def pronoun
-    if number == 1
-      "it"
-    else
-      "one"
-    end
+    "one"
   end
 
   def successor
-    if number == 0
-      99
-    else
-      number - 1
-    end
+    number - 1
+  end
+end
+
+class ContainerNumber0 < ContainerNumber
+  def successor
+    99
+  end
+  def action
+    "Go to the store and buy some more"
+  end
+  def amount
+    "no more"
+  end
+end
+
+class ContainerNumber1 < ContainerNumber
+  def pronoun
+    "it"
+  end
+  def container
+    "bottle"
+  end
+end
+
+class ContainerNumber6 < ContainerNumber
+  def container
+    "six-pack"
+  end
+  def amount
+    "1"
   end
 end
