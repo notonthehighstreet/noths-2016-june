5ce6530127c4227e8843a8d40e81c7ede726925c
Jamie Brown <jamiebrown@notonthehighstreet.com>
2016-06-27 12:41:59 +0100

    house shamless green

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -1,3 +1,69 @@
 class Bottles
+  def song
+    verses(99, 0)
+  end
 
+  def verses(upper, lower)
+    upper.downto(lower).map {|i| verse(i)}.join("\n")
+  end
+
+  def verse(number)
+      cn = ContainerNumber.new(number)
+      sn = ContainerNumber.new(cn.successor)
+      "#{cn.quantity.capitalize} #{cn.container} of beer on the wall, " +
+      "#{cn.quantity} #{cn.container} of beer.\n" +
+      "#{cn.action}, " +
+      "#{sn.quantity} #{sn.container} of beer on the wall.\n"
+  end
+
+  def containter_factory(number)
+    case number
+    when 0
+      ContainerNumber0
+    else
+      cont
+    end.new
+  end
+end
+
+class ContainerNumber
+  attr_reader :number
+
+  def initialize(number)
+    @number = number
+  end
+
+  def container
+    number == 1 ? "bottle" : "bottles"
+  end
+
+  def quantity
+    number == 0 ? "no more" : number.to_s
+  end
+
+  def pronoun
+    number == 1 ? "it" : "one"
+  end
+
+  def action
+    number == 0 ? "Go to the store and buy some more" : "Take #{pronoun} down and pass it around"
+  end
+
+  def successor
+    number == 0 ? "99" : number - 1
+  end
+end
+
+class ContainerNumber0
+  def action
+    "Go to the store and buy some more"
+  end
+
+  def successor
+    "99"
+  end
+
+  def quantity
+    "no more"
+  end
 end
--- house/lib/house.rb
+++ house/lib/house.rb
@@ -1,3 +1,32 @@
 class House
+  attr_reader :data
 
+  def initialize
+    @data = [
+              "the horse and the hound and the horn that belonged to",
+              "the farmer sowing his corn that kept",
+              "the rooster that crowed in the morn that woke",
+              "the priest all shaven and shorn that married",
+              "the man all tattered and torn that kissed",
+              "the maiden all forlorn that milked",
+              "the cow with the crumpled horn that tossed",
+              "the dog that worried",
+              "the cat that killed",
+              "the rat that ate",
+              "the malt that lay in",
+              "the house that Jack built.\n"
+            ]
+  end
+
+  def line(number)
+    "This is #{line_retrival(number)}"
+  end
+
+  def line_retrival(number)
+    data.last(number).join(' ')
+  end
+
+  def recite
+    1.upto(12).collect {|i| line(i)}.join("\n")
+  end
 end
